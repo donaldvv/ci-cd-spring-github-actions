9cb8de9252567475e37765929e62d9e8ba3b4a46
Donald Veizi <donald.veizi@lhind.dlh.de>
2024-03-12 21:52:48 +0100

    initial
    (cherry picked from commit 50a4f8f61b46e74238e311e3f280224d5c63dcc5)

---
+++ src/main/java/com/cicd/spring/CiCdSpringGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdSpringGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdSpringGithubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicd/spring/CiCdSpringGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdSpringGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
