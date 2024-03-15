c8920298198b2dfbf3cda254b0bf636fcc1cf8e4
Donald Veizi <donald.veizi@lhind.dlh.de>
2024-03-15 15:45:54 +0100

    MR

--- src/main/java/com/cicd/spring/CiCdSpringGithubActionsApplication.java
+++ src/main/java/com/cicd/spring/CiCdSpringGithubActionsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CiCdSpringGithubActionsApplication {
 
+    // I was here!
+
     public static void main(String[] args) {
         SpringApplication.run(CiCdSpringGithubActionsApplication.class, args);
     }
