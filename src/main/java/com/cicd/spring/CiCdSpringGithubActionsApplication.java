faccf6dc1586111867627297517ec08dd7a8d81f
Donald Veizi <donald.veizi@lhind.dlh.de>
2024-03-15 16:36:19 +0100

    MR

--- src/main/java/com/cicd/spring/CiCdSpringGithubActionsApplication.java
+++ src/main/java/com/cicd/spring/CiCdSpringGithubActionsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CiCdSpringGithubActionsApplication {
 
-    // I was here!
+    // I was here another time!
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdSpringGithubActionsApplication.class, args);
